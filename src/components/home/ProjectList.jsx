041f06291967fd2046fff3091b625c2f531ecd36
jubayer <jubayerahmed.dev@gmail.com>
2025-03-11 15:28:00 +0600

    project added ( whereIsIt )

--- src/components/home/ProjectList.jsx
+++ src/components/home/ProjectList.jsx
@@ -13,8 +13,8 @@ const ProjectList = [
     ],
     description:
       "Synapse is modern AI platform that focusing on giving smooth and interactive experience with a minimalistic user interface. It provides - payment integration, chat with AI and generate amazing images, etc.",
-    demo_url: "https://nexus-3e632.web.app/",
-    github_url: "https://github.com/jubayerahmed46/Nexus_Frontend",
+    demo_url: "https://synapse-776a2.web.app/",
+    github_url: "https://github.com/mr-jubayer/Synapse_frontend",
   },
   {
     id: 3,
@@ -34,72 +34,23 @@ const ProjectList = [
     demo_url: "https://nexus-3e632.web.app/",
     github_url: "https://github.com/jubayerahmed46/Nexus_Frontend",
   },
-  // {
-  //   id: 1,
-  //   name: "Crowdcube",
-  //   route: "crowdcube",
-  //   image: "https://i.ibb.co/LWhg24x/Captsdfasdure.png",
-  //   stack: [
-  //     { name: "React", color: "#4B9ABF" },
-  //     { name: "Tailwind CSS", color: "#0598A6" },
-  //     { name: "Firebase", color: "#FFB74D" },
-  //     { name: "Express.js", color: "#333333" },
-  //     { name: "MongoDB", color: "#3A9B2E" },
-  //   ],
-  //   description:
-  //     "Crowdcube is a funding platform that allows users to create fundraising campaigns for various causes and projects. It ensures secure transactions, real-time campaign tracking, and a seamless user experience.",
-  //   liveLink: "https://crowdcude-fb127.web.app/",
-  //   github: "https://github.com/jubayerahmed46/Crowdbube_frontend",
-  //   features: [
-  //     "🔐 Secure authentication with Firebase",
-  //     "🎯 Create and manage fundraising campaigns with real-time progress tracking",
-  //     "📊 Dashboard with insights on donation trends and campaign performance (almost done)",
-  //     "📣 Social media sharing features to boost campaign reach (working on)",
-  //   ],
-  //   challenges: [
-  //     "🛡 Ensuring secure transactions and preventing fraudulent donations",
-  //     "⚡ Optimizing performance for real-time campaign updates",
-  //     "📱 Making the UI mobile-friendly and responsive",
-  //   ],
-  //   goals: [
-  //     "📌 Expand globally to allow international fundraising",
-  //     "🤖 Introduce AI-based donation suggestions based on user interests",
-  //     "📱 Build a mobile app version with offline campaign tracking",
-  //   ],
-  // },
-  // {
-  //   id: 2,
-  //   name: "Where Is It?",
-  //   route: "where-is-it",
-  //   image: "https://i.ibb.co/WWDhL2SS/wherisit-PNG.png",
-  //   stack: [
-  //     { name: "React", color: "#4B9ABF" },
-  //     { name: "Tailwind CSS", color: "#0598A6" },
-  //     { name: "Firebase", color: "#FFB74D" },
-  //   ],
-  //   description:
-  //     "Where Is It? is a lost and found platform where users can report lost items, browse found items, and connect with others to recover their belongings. The platform uses real-time location tracking and filtering for efficient item discovery.",
-  //   liveLink: "https://whereisit-84e04.web.app/",
-  //   github:
-  //     "https://github.com/jubayerahmed46/WhereIsIt-frontend-mern-project-",
-  //   features: [
-  //     "📌 User-friendly lost and found submission form",
-  //     "🔍 Search and filter functionality to find lost items quickly",
-  //     "📅 Time-based notifications for nearby found items",
-  //     "👥 User-based accessibility.",
-  //     "📌 Save information for future problem and confusion",
-  //   ],
-  //   challenges: [
-  //     "🔍 Implementing an optimized search and filtering system",
-  //     "📲 Ensuring a responsive design for a smooth mobile experience",
-  //   ],
-  //   goals: [
-  //     "📍 Real-time location tracking for lost items using Google Maps API",
-  //     "🔮 Implement AI-powered item matching system",
-  //     "🔮 Expand database to cover nationwide lost & found reports",
-  //     "🔮 Introduce a chatbot assistant for instant lost item support",
-  //   ],
-  // },
+  {
+    id: 9,
+    title: "Where Is It?",
+    img: "https://i.ibb.co.com/7JN3yF30/whereisit-PNG.png",
+    tech_stack: [
+      "React",
+      "MongoDB",
+      "Tailwind CSS",
+      "Express.js",
+      "React Router",
+      "Firebase ",
+    ],
+    description:
+      "Where Is It? is a lost and found platform where users can report lost items, browse found items, and connect with others to recover their belongings. The platform uses real-time location tracking and filtering for efficient item discovery.",
+    demo_url: "https://whereisit-84e04.web.app/",
+    github_url: "https://github.com/mr-jubayer/WhereIsIt-frontend",
+  },
 ];
 
 export default ProjectList;
